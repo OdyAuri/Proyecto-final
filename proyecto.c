024ea39d61aed2193b4880e4399e6e0cb316060a
OdyAuri <ody.loa100@gmail.com>
2026-05-07 13:40:20 -0600

    Create proyecto.c

---
+++ proyecto.c
@@ -0,0 +1,9 @@
+// Inicalizar la matriz
+// Llenarla de ceros 
+// Agregarle valores 
+// COndicion de salidad donde el numero donde inicia es -1 terminara e lciclo
+// Usar while
+// Incisio 2 
+// Sunar todos lso votos de toda la matriz, es por eso qye se debe llenar primero con 0
+//Imprimir cancion numero 1 y 2 
+//Declarar contador de oyentes, para qu elos ciclos tenbgan un fin, usar un for 
