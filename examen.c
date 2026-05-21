0b2b86432e0119fd4a70b117645f13ed03efaf90
OdyAuri <ody.loa100@gmail.com>
2026-05-20 23:44:32 -0600

    Update examen.c

--- examen.c
+++ examen.c
@@ -6,116 +6,108 @@
 #define TOPE 10
 #define MIN 0
 
-int Valido;
-int PuntosCalc;
+int vo[MAX][3];
+int vc[TOPE] = {0}; 
+int po[MAX] = {0};
+int num = 0;
 
-void Bienvenida() 
-{
-    printf("¡COMIENZA EL CONCURSO! \n\n");
-    printf("Los oyentes ingresaran los números asignados a cada canción por la que votan \n\n");
-}
+int id_max1 = -1, max1 = -1;
+int id_max2 = -1, max2 = -1;
 
-void VotoValido(int v1, int v2, int v3) 
+void Leer() 
 {
-    if (v1 >= MIN && v1 < TOPE && v2 >= MIN && v2 < TOPE && v3 >= MIN && v3 < TOPE)
-        Valido = 1; //sí
-    else
-        Valido = 0; //no
-}
-
-void CalcularPuntos(int v1, int v2, int v3, int id1, int id2) 
-{
-    PuntosCalc = 0;
-    int bienprimera = (v1 == id1 || v2 == id1 || v3 == id1);
-    int biensegunda = (v1 == id2 || v2 == id2 || v3 == id2);
-    
-    if (bienprimera) PuntosCalc += 30;
-    if (biensegunda) PuntosCalc += 20;
-    if (bienprimera && biensegunda) PuntosCalc += 10;
-}
-
-int main() 
-{
-    int vo[MAX][3];
-    int vc[TOPE] = {0}; 
-    int po[MAX] = {0};
-    int num = 0;
     int v1 = MIN, v2 = 0, v3 = 0; 
     
-    Bienvenida();
+    printf("¡COMIENZA EL CONCURSO! \n\n");
+    printf("Los oyentes ingresaran los números asignados a cada canción por la que votan \n\n");
     
     while (!(v1 == -1) && num < MAX) 
     {
-        printf("Eres el oyente %d ¡RECUERDALO!\n", num);
-        printf("CANCIONES\n0. Locked Out of Heaven de Bruno Mars \n");
+        printf("\n\nCANCIONES\n0. Locked Out of Heaven de Bruno Mars \n");
         printf("1. the 1 de Taylor Swift \n2. Levels de Avicii \n");
-        printf("3. Shut up and Dance  de WALK THE MOON \n4. Tormenta (feat. Bad Bunny) de Gorillaz\n");
+        printf("3. Shut up and Dance de WALK THE MOON \n4. Tormenta (feat. Bad Bunny) de Gorillaz\n");
         printf("5. Pierre de Ryn Weaver \n6. Are you bored yet? (feat. Clairo) de Wallows\n");
-        printf("7. drop dead de Olivia Rodrigo \n8. Hot N cold de Katty Parry\n");
-        printf("9. This Love de Marron 5\n\n");
+        printf("7. drop dead de Olivia Rodrigo \n8. Hot N cold de Katy Perry\n");
+        printf("9. This Love de Maroon 5\n\n");
+        printf("Eres el oyente %d ¡RECUERDALO!\n", num);
         printf("Introduce los números de las 3 canciones por las que votas separados por espacios\n");
 
-
         scanf("%d %d %d", &v1, &v2, &v3);
+        
         char extra = ' ';
         while (extra != '\n') scanf("%c", &extra);
         
         if (v1 != -1) 
         {
-            VotoValido(v1, v2, v3);
-            
-            if (Valido) 
+            if ((v1 >= MIN && v1 < TOPE) && 
+                (v2 >= MIN && v2 < TOPE) && 
+                (v3 >= MIN && v3 < TOPE)) 
             {
-                vo[num][0] = v1; 
-                vo[num][1] = v2; 
-                vo[num][2] = v3;
-                vc[v1] += 3; 
-                vc[v2] += 2; 
-                vc[v3] += 1;
+                vo[num][0] = v1; vo[num][1] = v2; vo[num][2] = v3;
+                vc[v1] += 3; vc[v2] += 2; vc[v3] += 1;
                 num++;
             } 
-            else printf("ERROR \n ¡Las canciones solo van del 0 al 9 y son 3! \n Vuelve a intentar\n\n");
+            else 
+            {
+                printf("ERROR \n ¡Las canciones solo van del 0 al 9 y son 3! \n Vuelve a intentar\n\n");
+            }
         }
     }
-    
-    int id_max1 = -1, max1 = -1;
-    int id_max2 = -1, max2 = -1;
-    
+}
+
+void Top() 
+{
     for (int i = 0; i < TOPE; i++) 
     {
         if (vc[i] > max1) 
         { 
-        max2 = max1; 
-        id_max2 = id_max1; 
-        max1 = vc[i];
-        id_max1 = i; 
+            max2 = max1; id_max2 = id_max1; 
+            max1 = vc[i]; id_max1 = i; 
         }
-        else 
-        {
-            if (vc[i] > max2) 
-            { 
-            max2 = vc[i]; 
-            id_max2 = i; 
-            }
+        else if (vc[i] > max2) 
+        { 
+            max2 = vc[i]; id_max2 = i; 
         }
     }
     
+    printf("\nResultados de las votaciones\n");
+    printf("1a cancion: %d\n", id_max1);
+    printf("2a cancion: %d\n\n", id_max2);
+}
+
+void Repartir() 
+{
     int ganador = -1, maxp = -1;
     for (int i = 0; i < num; i++) 
     {
-        CalcularPuntos(vo[i][0], vo[i][1], vo[i][2], id_max1, id_max2);
-        po[i] = PuntosCalc;
+        int puntos = 0;
+        int bienprimera = (vo[i][0] == id_max1 || vo[i][1] == id_max1 || vo[i][2] == id_max1);
+        int biensegunda = (vo[i][0] == id_max2 || vo[i][1] == id_max2 || vo[i][2] == id_max2);
+        
+        if (bienprimera) puntos += 30;
+        if (biensegunda) puntos += 20;
+        if (bienprimera && biensegunda) puntos += 10;
+        
+        po[i] = puntos;
         
         printf("El oyente %d obtuvo %d puntos\n", i, po[i]);
         if (po[i] > maxp) 
         { 
-        maxp = po[i]; 
-        ganador = i; 
+            maxp = po[i]; ganador = i; 
         }
     }
     
-    if (num > 0) printf("\n¡El ganador es el oyente %d con %d puntos! \n\n ¡Felicidades oyente %d! :D\n", ganador, maxp, ganador);
-    else printf("\nNo hubo participantes :,( Nadie gana buu\n");
+    if (num > 0) 
+        printf("\n¡El ganador es el oyente %d con %d puntos! \n\n ¡Felicidades oyente %d! :D\n", ganador, maxp, ganador);
+    else 
+        printf("\nNo hubo participantes :,( Nadie gana buu\n");
+}
+
+int main() 
+{
+    Leer();
+    Top();
+    Repartir();
     
     return 0;
 }
